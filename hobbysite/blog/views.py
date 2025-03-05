00b41fd8b4815559e74f26377443d59bbf40dfb3
loltreeman <naguio.greg@gmail.com>
2025-03-05 12:54:40 +0800

    Added more views to Articles, and Article Categories

--- hobbysite/blog/views.py
+++ hobbysite/blog/views.py
@@ -1,3 +1,29 @@
+from django.views.generic import ListView, DetailView
+from django.http import HttpResponse 
 from django.shortcuts import render
+from .models import Article, ArticleCategory
 
-# Create your views here.
+# Create your views here 
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'blog/article_list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'blog/article_detail.html'
+
+def article_list(request):
+    articles = Article.objects.all()
+    ctx = {"articles_list": articles}
+    return render(request, "blog/article_list.html", ctx)
+
+def article(request, pk):
+    article = Article.objects.get(pk=pk)
+    ctx = {
+        "name": article.title,
+        "category": article.category.name,
+        "entry": article.entry,
+        "created_on": article.created_on,
+        "updated_on": article.updated_on,
+    }
+    return render(request, "blog/article_detail.html", ctx)
