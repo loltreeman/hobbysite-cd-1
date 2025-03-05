98ca0c39504bf7efb66e9528ef164ebc269313bd
loltreeman <naguio.greg@gmail.com>
2025-03-05 13:16:21 +0800

    Added model for Article

--- hobbysite/blog/models.py
+++ hobbysite/blog/models.py
@@ -12,3 +12,19 @@ class ArticleCategory(models.Model):
     def __str__(self):
         return self.name
 
+class Article(models.Model):
+    title = models.CharField(max_length=255)
+    category = models.ForeignKey(ArticleCategory, on_delete=models.SET_NULL, null=True, related_name="article_category")
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True, null=True)
+    updated_on = models.DateTimeField(auto_now=True, null=True)
+
+    # Articles should be sorted by the date it was created, in descending order
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:article_detail", args=[str(self.pk)])
